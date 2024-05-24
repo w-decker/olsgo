40c4ffe52824d35ec19d38ebc88119bfeea9902a
Will <deckerwill7@gmail.com>
2024-05-24 12:08:47 -0400

    add a few more calculation funcs --> TO DO: adjust for memory handeling

--- computations.go
+++ computations.go
@@ -2,8 +2,16 @@ package olsgo
 
 import "math"
 
+// output
+type ols struct {
+	intercept float64
+	b1        float64
+	r         float64
+	R2        float64
+}
+
 // sum over 1d slice
-func sum(x []float64) float64 {
+func Sum(x []float64) float64 {
 	sum := 0.0
 	for i := 0; i < len(x); i++ {
 		sum += float64(x[i])
@@ -12,30 +20,41 @@ func sum(x []float64) float64 {
 }
 
 // compute mean over 1d slice
-func mean(x []float64) float64 {
+func Mean(x []float64) float64 {
 	n := float64(len(x))
-	s := sum(x)
+	s := Sum(x)
 	avg := s / n
 
 	return avg
 }
 
+// Standardize 1d slize
+func ZScore(x []float64) []float64 {
+	var z []float64
+	mu := Mean(x)
+	sigma := Std(x)
+	for _, n := range x {
+		z = append(z, ((n - mu) / sigma))
+	}
+	return z
+}
+
 // compute total sums of squares for 1d slice
-func tss(x []float64) float64 {
+func Tss(y []float64) float64 {
 	var t []float64
-	mu := mean(x)
-	for _, v := range x {
+	mu := Mean(y)
+	for _, v := range y {
 		d := v - mu
 		t = append(t, d*d)
 	}
 
-	ss := sum(t)
-	return ss
+	sst := Sum(t)
+	return sst
 }
 
 // compute std over 1d slice
-func std(x []float64) float64 {
-	numer := tss(x)
+func Std(x []float64) float64 {
+	numer := Tss(x)
 	n := float64(len(x))
 
 	sigma := math.Sqrt((numer / n))
@@ -43,7 +62,8 @@ func std(x []float64) float64 {
 	return sigma
 }
 
-func pearsonr(x []float64, y []float64) float64 {
+// Compute Pearson's r for two 1d slices.
+func PearsonR(x []float64, y []float64) float64 {
 
 	n := float64(len(x))
 	m := make([]float64, len(x))
@@ -51,7 +71,62 @@ func pearsonr(x []float64, y []float64) float64 {
 		m = append(m, n*float64(y[idx]))
 	}
 
-	numer := sum(m)
+	numer := Sum(m)
 
 	return numer/n - 1
 }
+
+func B1(x []float64, y []float64) float64 {
+	r := PearsonR(x, y)
+	sx := Std(x)
+	sy := Std(y)
+
+	b := r * (sy / sx)
+
+	return b
+}
+
+func Intercept(x []float64, y []float64) float64 {
+	b1 := B1(x, y)
+	xbar := Mean(x)
+	ybar := Mean(y)
+
+	i := ybar - (b1 * xbar)
+
+	return i
+}
+
+// Compute y-hat
+func YHat(x []float64, y []float64) []float64 {
+	inter := Intercept(x, y)
+	b1 := B1(x, y)
+	yhat := make([]float64, len(x))
+	for i := range x {
+		yhat[i] = inter + b1*x[i]
+	}
+	return yhat
+}
+
+// compute sums of squares explained for 1d slice
+func Sse(y []float64, yhat []float64) float64 {
+	var t []float64
+	ybar := Mean(y)
+	for i := range y {
+		d := yhat[i] - ybar
+		t = append(t, d*d)
+	}
+	sse := Sum(t)
+	return sse
+}
+
+func R2(x []float64, y []float64) float64 {
+	sst := Tss(y)
+	yhat := YHat(x, y)
+	sse := Sse(y, yhat)
+
+	r2 := sse / sst
+	return r2
+}
+
+// Compute ordinary-least-squares regression over x and y variable
+// func OLS(d map[string][]float64, x string, y string)
