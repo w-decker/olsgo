c7111c8b2f706111216221258c37849554027f9e
Will <deckerwill7@gmail.com>
2024-05-28 19:42:56 -0500

    add some other stuff

--- example/main.go
+++ example/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/w-decker/olsgo"
@@ -14,14 +15,21 @@ func main() {
 		log.Fatalf("Error: %v \nCannot load file.")
 	}
 
+	// normalize
+	d["wt"] = olsgo.ZScore(d["wt"])
+	d["ht"] = olsgo.ZScore(d["ht"])
+
 	// compute O-L-S
-	reg := olsgo.OLS(d, "wt", "ht")
+	reg := olsgo.OLS(d, "wt", "ht") // out := olsgo.OLS(data (output from olsgo.LoadCsv), x-variable, y-variable)
+
+	// print output to console
+	fmt.Println(reg.Format())
 
 	// save
 	reg.Save("output")
 
 	// plot raw
-	err = olsgo.PlotRaw("height_v_weight.csv", "plot.png", "wt", "ht")
+	err = olsgo.PlotRaw("height_v_weight.csv", "plot3.png", "wt", "ht")
 	if err != nil {
 		log.Fatalf("Error: %v \nCannot render plot.")
 	}
